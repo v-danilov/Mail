8c1ddf2e8f1508f825790c0ee79e861e34eac307
Vadim <bounce_man@live.com>
2016-09-20 18:04:47 +0300

    Added scene builder and logon window

--- src/code/Controller.java
+++ src/code/Controller.java
@@ -1,4 +1,5 @@
-package sample;
+package code;
 
 public class Controller {
+
 }
---
+++ src/code/Main.java
@@ -0,0 +1,27 @@
+package code;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent logOn = FXMLLoader.load(getClass().getResource("../view/LogOn.fxml"));
+        primaryStage.setTitle("JMP");
+        primaryStage.setScene(new Scene(logOn));
+        primaryStage.show();
+        //Parent mainWindow = FXMLLoader.load(getClass().getResource("../view/ApplicationWindow.fxml"));
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(mainWindow, 300, 275));
+        //primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
